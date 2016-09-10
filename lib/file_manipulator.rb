fb423f9e50ed3ef8cf887d08093f642a44f8411e
Atsushi Ishida <gipcompany@gmail.com>
2016-09-10 23:36:29 +0900

    create binstubs

--- lib/file_manipulator.rb
+++ lib/file_manipulator.rb
@@ -1,5 +1,10 @@
+require "file_manipulator/splitter"
 require "file_manipulator/version"
 
 module FileManipulator
-  # Your code goes here...
+  module_function
+
+  def split(file_name, output_directory, size)
+    Splitter.new(file_name, output_directory, size).run
+  end
 end
