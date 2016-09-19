0422b59f3d34cc3d36d3fa43ec3566e99f91fd10
Atsushi Ishida <gipcompany@gmail.com>
2016-09-19 11:35:23 +0900

    update rspec

--- spec/file_manipulator/splitter_spec.rb
+++ spec/file_manipulator/splitter_spec.rb
@@ -2,12 +2,11 @@ require "spec_helper"
 
 module FileManipulator
   describe Splitter do
-    let(:basename) { "foo" }
-    let(:extname) { "txt" }
     let(:output_directory) { File.join(Dir.pwd, 'tmp') }
     let(:splitter) { Splitter.new }
+
     let!(:file) do
-      path = File.join(Dir.pwd, 'tmp', "#{basename}.#{extname}")
+      path = File.join(Dir.pwd, 'tmp', "foo.txt")
       _ = File.new(path, 'w')
       _.write("123456789\n" * 1_000)
       _.rewind
@@ -17,16 +16,15 @@ module FileManipulator
     before do
       FileManipulator.configure do |config|
         config.prefix = 'file_manipulator'
-        config.file_name = File.join(Dir.pwd, 'tmp', "#{basename}.#{extname}")
+        config.file_name = File.join(Dir.pwd, 'tmp', "foo.txt")
         config.output_directory = File.join(Dir.pwd, 'tmp')
         config.size = 10
       end
     end
 
     after do
-      Dir::glob(File.join(output_directory, "*.#{extname}")).each do |_file|
-        File.delete(_file)
-      end
+      Dir::glob(File.join(output_directory, "file_manipulator_*")).each { |_| File.delete(_) }
+      File.delete(FileManipulator.configuration.file_name)
     end
 
     it '#initialize' do
@@ -41,11 +39,11 @@ module FileManipulator
     end
 
     it '#basename' do
-      expect(splitter.send(:basename)).to eq(basename)
+      expect(splitter.send(:basename)).to eq('foo')
     end
 
     it '#extname' do
-      expect(splitter.send(:extname)).to eq(extname)
+      expect(splitter.send(:extname)).to eq('txt')
     end
 
     it '#file_name' do
@@ -56,16 +54,18 @@ module FileManipulator
       expect(splitter.send(:number_of_digits)).to eq(4)
     end
 
-    describe '#output_file_name' do
-      it 'with extension' do
-        index = 1
-        expect(splitter.send(:output_file_name, index)).to eq("file_manipulator_foo_000#{index}.txt")
-      end
+    it '#output_file_name, with extension' do
+      index = 1
+      expect(splitter.send(:output_file_name, index)).to eq("file_manipulator_foo_000#{index}.txt")
+    end
 
-      it 'with no extension' do
-        pending 'TODO: '
-        expect(splitter.send(:output_file_name, index)).to eq("file_manipulator_foo_000#{index}")
-      end
+    it '#output_file_name, with no extension' do
+      basename = File.basename(splitter.config.file_name, '.txt')
+      filename = File.join(Dir.pwd, 'tmp', basename)
+      FileUtils.mv(splitter.config.file_name, filename)
+      splitter.config.file_name = filename
+      index = 1
+      expect(splitter.send(:output_file_name, index)).to eq("file_manipulator_foo_000#{index}")
     end
 
     it '#size' do
--- spec/file_manipulator_spec.rb
+++ spec/file_manipulator_spec.rb
@@ -9,22 +9,36 @@ describe FileManipulator do
     end
   end
 
-  describe '#configure' do
+  describe 'class methods' do
     before do
       FileManipulator.configure do |config|
         config.prefix = 'file_manipulator'
-        config.file_name = 'Gemfile.lock'
+        config.file_name = 'LICENSE.txt'
         config.output_directory = 'tmp'
         config.size = 1
       end
     end
 
-    it "configure can set the configuration values" do
-      splitter = FileManipulator::Splitter.new
+    it ".configuration" do
+      configuration = FileManipulator.configuration
 
-      expect(splitter.config.file_name).to eq('Gemfile.lock')
-      expect(splitter.config.output_directory).to eq('tmp')
-      expect(splitter.config.size).to eq(1)
+      expect(configuration.file_name).to eq('LICENSE.txt')
+      expect(configuration.output_directory).to eq('tmp')
+      expect(configuration.size).to eq(1)
+    end
+
+    it ".reset" do
+      configuration = FileManipulator.reset
+
+      expect(configuration.file_name).to eq(nil)
+      expect(configuration.output_directory).to eq(nil)
+      expect(configuration.size).to eq(nil)
+    end
+
+    it ".split" do
+      FileManipulator.split
+      splitted_files = Dir.glob("tmp/*.txt")
+      expect(splitted_files).not_to eq([])
     end
   end
 end
