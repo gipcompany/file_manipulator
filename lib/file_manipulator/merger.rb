047163808befc065655e4a6b81d9233d92415b7f
Atsushi Ishida <gipcompany@gmail.com>
2016-09-20 11:18:43 +0900

    implement merge method #4

--- lib/file_manipulator/merger.rb
+++ lib/file_manipulator/merger.rb
@@ -7,6 +7,21 @@ module FileManipulator
     end
 
     def run
+      string_io = StringIO.new
+
+      Dir.glob("#{config.split_files_directory}/#{config.prefix}_*").sort.each do |file|
+        string_io.puts File.read(file)
+      end
+
+      File.write(merged_file, string_io.string)
+    end
+
+    def merged_file
+      sample_split_file = Dir.glob("#{config.split_files_directory}/#{config.prefix}_*").first
+      basename = File.basename(sample_split_file)
+      extname = File.extname(sample_split_file)
+      basename = basename.sub(/^#{config.prefix}_/, '').sub(/#{extname}$/, '').sub(/_\d*$/, '')
+      File.join(config.merged_file_directory, "#{basename}#{extname}")
     end
   end
 end
--- spec/file_manipulator/merger_spec.rb
+++ spec/file_manipulator/merger_spec.rb
@@ -14,16 +14,24 @@ module FileManipulator
         config.prefix = 'file_manipulator'
         config.file_name = dummy_txt.path
         config.split_files_directory = File.join(Dir.pwd, 'tmp')
+        config.merged_file_directory = File.join(Dir.pwd, 'tmp')
         config.size = 1000
       end
+
+      FileManipulator.split
     end
 
     after do
-      Dir::glob(File.join(File.join(Dir.pwd, 'tmp'), "file_manipulator_*")).each { |_| File.delete(_) }
+      Dir::glob(File.join(File.join(Dir.pwd, 'tmp'), "*")).each { |_| File.delete(_) }
     end
 
     it '#initialize' do
       expect(merger.config.class).to eq(Configuration)
+    end
+
+    it '#run' do
+      merger.run
+      expect(File.read(merger.config.file_name)).to eq(File.read(merger.merged_file))
     end
   end
 end
